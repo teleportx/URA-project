28bd5d8326ad3cf80f9fc28ef0e967a84618d1eb
teleportx <stepan@xstl.ru>
2024-02-05 13:15:28 +0300

    fix can't restrict chat owner

--- handlers/srat.py
+++ handlers/srat.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 
+import aiogram
 from aiogram import types, Router, F
 from aiogram.enums import ChatType
 from aiogram.types import ChatPermissions
@@ -63,8 +64,13 @@ async def send_srat(message: types.Message, user: User):
     user.sret = datetime.now() if sret else None
     user.save()
 
-    # permissions = ChatPermissions(can_send_messages=sret)
-    # await config.Telegram.bot.restrict_chat_member(config.Telegram.group_id, message.from_user.id, permissions)
+    try:
+        permissions = ChatPermissions(can_send_messages=sret)
+        await config.Telegram.bot.restrict_chat_member(config.Telegram.group_id, message.from_user.id, permissions)
+
+    except aiogram.exceptions.TelegramBadRequest as e:
+        if e.message != 'Bad Request: can\'t remove chat owner':
+            raise e
 
     for send_to in User.select():
         try:
