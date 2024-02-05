02ab3e3f2843fdce7523c582db94a36c748c0f9c
teleportx <stepan@xstl.ru>
2024-02-05 13:35:39 +0300

    refactor

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,19 +1,13 @@
 from aiogram import Router
 
-from handlers import start
-from handlers import srat
-from handlers import notify
-from handlers import user_control
-from handlers import settings
-from handlers import group_perdish
-from handlers import link
+from . import start
+from . import srat
+from . import other_commands
+from . import group_perdish
 
 router = Router()
 
 router.include_router(start.router)
 router.include_router(srat.router)
-router.include_router(notify.router)
-router.include_router(user_control.router)
-router.include_router(settings.router)
 router.include_router(group_perdish.router)
-router.include_router(link.router)
+router.include_router(other_commands.router)
