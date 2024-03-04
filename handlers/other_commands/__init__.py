d4ed82f3f7776b036be44719850c6934f1b687e4
teleportx <stepan@xstl.ru>
2024-03-04 20:50:28 +0300

    remove unused commands

--- handlers/other_commands/__init__.py
+++ handlers/other_commands/__init__.py
@@ -1,13 +1,9 @@
 from aiogram import Router
 
-from . import link
 from . import notify
-from . import settings
 from . import user_control
 
 router = Router()
 
-router.include_router(link.router)
 router.include_router(notify.router)
-router.include_router(settings.router)
 router.include_router(user_control.router)
