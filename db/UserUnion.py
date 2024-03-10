eaea914bca13782d37647767597e071f8cfac03e
teleportx <stepan@xstl.ru>
2024-03-10 21:25:17 +0300

    group control

--- db/UserUnion.py
+++ db/UserUnion.py
@@ -3,17 +3,20 @@ from random import randint
 from tortoise import fields
 from tortoise.models import Model
 
+from db.fields import AutoNowDatetimeField
+
 
 class Group(Model):
-    name = fields.CharField(max_length=32)
-    owner = fields.ForeignKeyField('models.User')
-
-    members = fields.ManyToManyField('models.User')
-
-    notify_perdish = fields.BooleanField(default=True)
-    password = fields.IntField()
-
     @staticmethod
     def generate_password() -> int:
         return randint(100000, 999999)
 
+    name = fields.CharField(max_length=32)
+    owner = fields.ForeignKeyField('models.User', related_name='groups_owned')
+
+    members = fields.ManyToManyField('models.User')
+
+    notify_perdish = fields.BooleanField(default=True)
+    password = fields.IntField(default=generate_password)
+
+    created_at = AutoNowDatetimeField()
