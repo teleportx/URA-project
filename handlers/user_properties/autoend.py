0ffb4e9c71aa5a825bc0f5e4e58f5fe6b95f8d45
teleportx <stepan@xstl.ru>
2024-05-07 23:35:00 +0300

    custom autoend time command

--- handlers/user_properties/autoend.py
+++ handlers/user_properties/autoend.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram import types
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 
 from db.User import User
 
@@ -13,3 +13,23 @@ async def switchdefaultautoend(message: types.Message, user: User):
     await user.save()
 
     await message.reply(f'*{["Выключили", "Включили"][user.autoend]}* автозавершение по умолчанию.')
+
+
+@router.message(Command("setautoendtime"))
+async def setautoendtime(message: types.Message, command: CommandObject, user: User):
+    args = command.args
+    if args is None:
+        args = ''
+
+    if not args.isnumeric():
+        await message.reply('Пример:\n`/setautoendtime <time in minutes>`')
+        return
+
+    if not(3 <= int(args) <= 60):
+        await message.reply('Время автозавершения должно быть *не меньше 3 и не больше 60 минут.*')
+        return
+
+    user.autoend_time = int(args)
+    await user.save()
+
+    await message.reply(f'Теперь ваше время автозавершения сранья: *{args} минут*!')
