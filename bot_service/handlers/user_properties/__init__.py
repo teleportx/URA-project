997e24a6ce6249f5b4a787b1fe479cae8b33307c
Stepan Khozhempo <stepan@khoz.ru>
2024-07-22 16:49:34 +0300

    add user analytics

--- bot_service/handlers/user_properties/__init__.py
+++ bot_service/handlers/user_properties/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from . import setnickname
 from . import autoend
+from . import analytics
 
 router = Router()
 
 router.include_router(setnickname.router)
 router.include_router(autoend.router)
+router.include_router(analytics.router)
---
+++ bot_service/handlers/user_properties/analytics.py
@@ -0,0 +1,92 @@
+from aiogram import Router
+from aiogram import types
+from aiogram.filters import Command, CommandObject
+from asyncpg import Record
+from tortoise import Tortoise
+
+import config
+from db.ToiletSessions import SretSession
+from db.User import User
+
+router = Router()
+
+stat_request = '''SELECT sret_type,
+                            autoend,    
+                            start >= (now() - interval '1 week') as last_week,
+                            start >= (now() - interval '1 month') as last_month,
+                            AVG(s.end - s.start),
+                            COUNT(*)
+                  FROM sretsession as s
+                           WHERE user_id = %d   
+                           GROUP BY sret_type, last_week, last_month, autoend;'''
+
+
+def calc_avg(var, el):
+    seconds =  el['avg'].total_seconds()
+    if var[0] is None:
+        var = (seconds, el['count'])
+
+    else:
+        total = var[1] + el['count']
+        var = ((var[0] * var[1] + seconds * el['count']) / total, total)
+
+    return var
+
+
+def render_time(val):
+    if val[0] is None:
+        return 'Н/Д'
+
+    return f'{round(val[0] / 60, 1)} минут'
+
+
+@router.message(Command("anal"))
+async def anal(message: types.Message, user: User):
+    conn = Tortoise.get_connection('default')
+    res = await conn.execute_query(stat_request % user.uid)
+
+    co = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+    avg = [(None, 0), (None, 0), (None, 0)]
+    for el in res[1]:
+        crit = not el['autoend'] and el['sret_type'] != 3
+        if el['last_week']:
+            co[0][el['sret_type'] - 1] += el['count']
+            if crit:
+                avg[0] = calc_avg(avg[0], el)
+
+        if el['last_month']:
+            co[1][el['sret_type'] - 1] += el['count']
+            if crit:
+                avg[1] = calc_avg(avg[1], el)
+
+        co[2][el['sret_type'] - 1] = el['count']
+        if crit:
+            avg[2] = calc_avg(avg[2], el)
+
+    text = (
+            f'<b>Всего за все время вы:</b>\n'
+            f'Раз срали: <code>{co[0][0]}</code>\n'
+            f'Раз дристали: <code>{co[0][1]}</code>\n'
+            f'Пернули: <code>{co[0][2]}</code>\n'
+            f'Среднее время в туалете: <code>{render_time(avg[0])}</code>\n\n'
+            
+            f'<b>За последний месяц вы:</b>\n'
+            f'Раз срали: <code>{co[1][0]}</code>\n'
+            f'Раз дристали: <code>{co[1][1]}</code>\n'
+            f'Пернули: <code>{co[1][2]}</code>\n'
+            f'Среднее время в туалете: <code>{render_time(avg[1])}</code>\n\n'
+            
+            f'<b>За последнюю неделю вы:</b>\n'
+            f'Раз срали: <code>{co[2][0]}</code>\n'
+            f'Раз дристали: <code>{co[2][1]}</code>\n'
+            f'Пернули: <code>{co[2][2]}</code>\n'
+            f'Среднее время в туалете: <code>{render_time(avg[2])}</code>\n\n'
+            
+            f'Аккаунт создан: <code>{user.created_at}</code>\n'
+    )
+
+    await message.reply(text)
