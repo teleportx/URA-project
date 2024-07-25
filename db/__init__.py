0ba85a0d5c68247586eec97ba6cbb2b2a36e1b05
Stepan Khozhempo <stepan@khoz.ru>
2024-07-25 21:12:41 +0300

    fix db

--- db/__init__.py
+++ db/__init__.py
@@ -5,19 +5,8 @@ from tortoise import Tortoise
 from config import DB as cDB
 
 
-TORTOISE_ORM = {
-    "connections": {"default": f"postgres://{cDB.user}:{urllib.parse.quote_plus(cDB.password)}@{cDB.host}:{cDB.port}/{cDB.db_name}"},
-    "apps": {
-        "models": {
-            "models": ['db.User', 'db.ToiletSessions', 'db.UserUnion', 'db.Notify', 'db.ApiAuth', 'aerich.models'],
-            "default_connection": "default",
-        },
-    },
-}
-
-
 async def init():
     await Tortoise.init(
-        db_url=TORTOISE_ORM['connections']['default'],
-        modules={"models": TORTOISE_ORM['apps']['models']['models']}
+        db_url=f"postgres://{cDB.user}:{urllib.parse.quote_plus(cDB.password)}@{cDB.host}:{cDB.port}/{cDB.db_name}",
+        modules={"models": ['db.User', 'db.ToiletSessions', 'db.UserUnion', 'db.Notify', 'db.ApiAuth']}
     )
